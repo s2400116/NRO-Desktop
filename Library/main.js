ee21aaf6d36dd85720fe0e2cc0c82eb092153a5a
student116 <S2400116@students.villacollege.edu.mv>
2025-04-04 19:43:44 +0500

    Music Player Expansion & UI Enhancements

--- Library/main.js
+++ Library/main.js
@@ -2,9 +2,13 @@ const contextMenu = document.getElementById('ContextMenu')
 const application = document.getElementById('Application')
 const game = document.getElementsByClassName('Game')
 const music = document.getElementById("Music")
+let musicList = 7
+let repeat = "false"
+let changeVolume = 0.25
 
 music.play()
-music.volume = 0.3
+music.volume = changeVolume
+temp = "1L"
 
 document.addEventListener("contextmenu", (event) => {
     event.preventDefault()
@@ -25,13 +29,6 @@ document.getElementById("searchBar").addEventListener("keypress", function(event
         this.value = ""
     }
 });
-setInterval (() => {
-    if (music.ended) {
-        setTimeout(() => {
-            music.play()
-        }, 9000)
-    }
-}, 1000)
 
 function executeCommand(command) {
     if (command.startsWith("/bg")) {
@@ -42,11 +39,17 @@ function executeCommand(command) {
         changeFont(varColor);
     } else if (command.startsWith("/msc.vol")) {
         let varVolume = command.replace("/msc.vol", "").trim();
-        varVolume = varVolume/100
-        music.volume = parseFloat(varVolume);
+        music.volume = parseFloat(varVolume/100);
+        console.log(music.volume)
+        Song()
     } else if (command.startsWith("/msc.c")) {
         let trackNumber = command.replace("/msc.c", "").trim();
         changeMusic(trackNumber);
+    } else if (command.startsWith("/msc.rep")) {
+        repeat = command.replace("/msc.rep", "").trim();
+    } else if (command.startsWith("/msc.l")) {
+        temp += "L"
+        console.log (temp)
     } else {
         alert("Unknown command: " + command);
     }
@@ -60,8 +63,21 @@ function changeBackground(bgNumber) {
 function changeFont(varColor){
     document.body.style.color = varColor
 }
+function onEnded() {
+    setTimeout(() => {
+        if (repeat == "false") {
+            temp++
+        }
+        if (temp >= musicList) {
+            temp = 1
+        }
+        let format = String(temp).padStart(2, '0')
+        changeMusic(format)
+    }, 10000)
+}
 function changeMusic(trackNumber) {
     let Track = `Library/Audio/bgm ${trackNumber}.mp3`;
+    temp = trackNumber
     music.src = Track;
     music.play();
 }
@@ -70,6 +86,46 @@ function BGM() {
         music.play()
     } else {
         music.pause()
+    }
+}
+function BGMB() {
+    temp = temp.replace("L", "").trim();
+    temp--
+    if (temp <= 0) {
+        temp = musicList - 1
+    }
+    let format = String(temp).padStart(2, '0')
+    changeMusic(format)
+}
+function BGMF() {
+    temp = temp.replace("L", "").trim();
+    temp++
+    if (temp >= musicList) {
+        temp = 1
+    }
+    let format = String(temp).padStart(2, '0')
+    changeMusic(format)
+}
+function Repeat() {
+    repeat = repeat === "false" ? "true":"false"
+}
+function Volume() {
+    changeVolume += 0.25
+    if (changeVolume > 1) {
+        changeVolume = 0
+    }
+    music.volume = changeVolume
+    Song()
+    console.log("Volume", music.volume)
+}
+function Song() {
+    let vol = document.getElementById("Vol")
+    if (music.volume > 0.5) {
+        vol.src = "Library/Icon/HighVolume.png"
+    } else if (music.volume > 0.0) {
+        vol.src = "Library/Icon/LowVolume.png"
+    } else {
+        vol.src = "Library/Icon/Mute.png"
     }
 }
 function Exit() {
@@ -101,6 +157,7 @@ function Close() {
 }
 function TextDocument() {
     application.style.display = "block"
+    application.style.backgroundColor = "var(--hover-clr1)"
     Exit()
     document.querySelectorAll(".TxtDoc").forEach(el => {
         el.style.display = "block"
@@ -108,6 +165,7 @@ function TextDocument() {
 }
 function Browser(){
     application.style.display = "block"
+    application.style.backgroundColor = "var(--hover-clr)"
     Exit()
     document.querySelectorAll(".Bro").forEach(el => {
         el.style.display = "block"
@@ -126,4 +184,14 @@ function Folder() {
     document.querySelectorAll(".Folder").forEach(el => {
         el.style.display = "block"
     });
+}
+function Notif() {
+    document.getElementById("MusicPlayer").style.display = "none"
+    let temp = document.getElementById("Notif")
+    temp.style.display = temp.style.display === "none" ? "Block":"None";
+}
+function Music() {
+    document.getElementById("Notif").style.display = "none"
+    let temp = document.getElementById("MusicPlayer")
+    temp.style.display = temp.style.display === "none" ? "block":"None";
 }
